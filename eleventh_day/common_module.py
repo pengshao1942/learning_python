c482daab76ac5673b4d9b6df958129b04abd6117
pengshao1942 <shaopeng1942@163.com>
2020-08-21 17:27:02 +0800

    functools模块

--- eleventh_day/common_module.py
+++ eleventh_day/common_module.py
@@ -1657,6 +1657,118 @@ for e in it.combinations_with_replacement('ABCD', 2):
 
 
 
+#functools模块的功能函数：包含了一些函数装饰器和便捷的功能函数
+'''查看functools模块的功能函数
+>>import functools
+>>[e for e in dir(functools) if not e.startswith('_')]
+'''
+
+#常用的函数装饰器和功能函数
+"""
+functools.cmp_to_key(func):将老式的比较函数(func)转换为关键字函数(key function)。
+    在Python3中比较大小、排序都是基于关键字函数的，Python3不支持老式的比较函数
+@functools.lru_cache(maxsize=128, typed=False):该函数装饰器使用LRU(最近最少使用)缓存算法来缓存相对
+    耗时的函数结果，避免传入相同的参数重复计算。同时，缓存并不会无限增长，不用的缓存会被释放。其中maxsize
+    参数用于设置缓存占用的最大字节数，typed参数用于设置将不同类型的缓存结果分开存放
+@functools.total_ordering: 这个类装饰器(作用类似于函数装饰器，只是它用于修饰类)，用于为类自动生成比较方法。
+    通常来说，开发者只要提供__lt__()、__le__()、__gt__()、__ge__()其中之一(最好能提供__eq__()方法)，
+    @functools.total_ordering装饰器就会为该类生成剩下的比较方法
+functools.partial(func, *args, **keywords):该函数用于为func函数的部分参数指定参数值,从而得到一个转换后的函数，
+    程序以后调用转换后的函数时，就可以少传入那些已指定值的参数
+functools.partialmethod(func, *args, **keywords): 该函数与上一个函数的含义完全相同，只不过该函数用于为类中的
+    方法设置参数值
+functools.reduce(function, iterable[, initializer]): 将初始值(默认为0,可由initializer参数指定)、迭代器的当
+    前元素传入function函数，将计算出来的函数结果作为下一次计算的初始值、迭代器的下一个元素再次调用function函数
+    ......依次类推,直到迭代器的最后一个元素
+@functools.singledispatch: 该函数装饰器用于实现函数对多个类型进行重载。比如同样的函数名称，为不同的参数类型提供
+    不同的功能实现。该函数的本质就是根据参数类型的变换，将函数转向调用不同的函数
+functools.update_wrapper(wrapper, wrapped, assigned=WRAPPER_ASSIGNMENTS, updated=WRAPPER_UPDATES):对 wrapper 函数进行包装，使之
+    看上去就像wrapped(被包装)函数，是函数
+@functools.wraps(wrapped, assigned=WRAPPER_ASSIGNMENTS, updated=WRAPPER_UPDATES)：该函数装饰器用于修饰包装函数，使包装函数
+    看上去就像wrapped函数，是函数装饰器
+@functools.wraps(wrapped, assigned=WRAPPER_ASSIGNMENTS, updated=WRAPPER_UPDATES)： 该函数装饰器用于修饰包装函数,使包装函数
+    看上去就像wrapped(被包装)函数
+"""
+
+#示例：示范部分函数或函数装饰器的用法
+'''
+from functools import *
+
+#设初始值(默认为0)为x,当前序列元素为y，将x+y的和作为下一次计算的初始值
+print(reduce(lambda x,y: x + y, range(5)))
+print(reduce(lambda x,y: x + y, range(6)))
+
+#设初始值为10
+print(reduce(lambda x,y: x + y, range(6), 10))
+print('--------------------')
+
+class User:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return 'User[name = %s' % self.name
+
+#定义一个老式的大小比较函数，User的name越长，该User越大
+def old_cmp(u1, u2):
+    return len(u1.name) - len(u2.name)
+
+my_data = [User('Kotlin'), User('Swift'), User('Go'), User('Java')]
+#对my_data排序，需要关键字函数(调用cmp_to_key将old_cmp转换为关键字函数)
+my_data.sort(key=cmp_to_key(old_cmp))
+print(my_data)
+print('----------------')
+@lru_cache(maxsize=32)
+
+def factorial(n):
+    print('~~计算%d的阶乘~~' % n)
+    if n == 1:
+        return  1
+    else: 
+        return n * factorial(n -1)
+#只有这行会计算，然后会缓存5、4、3、2、1的阶乘
+print(factorial(5))
+print(factorial(3))
+print(factorial(5))
+print('----------------')
+#int函数默认将十进制形式的字符串转换为整数
+print(int('12345'))
+#为int函数的base参数指定参数值
+basetwo = partial(int, base=2)
+basetwo.__doc__ = '将二进制形式的字符串转换成整数'
+#相当于执行base为2的int函数,下面2行代码本质一样
+print(basetwo('10010'))  
+print(int('10010', 2))
+'''
+
+
+"""partialmethod()与partial()函数的作用基本相似，区别是: partial()函数用于为函数的部分参数绑定值；而partialmethod()函数则用于为类中方法的部分参数绑定值"""
+
+#示范partialmethod()函数的用法
+from functools import *
+class Cell:
+    def __init__(self):
+        self._alive = False
+    # @property装饰器指定该方法可使用属性语法访问
+    @property
+    def alive(self):
+        return self._alive
+    def set_state(self, state):
+        self._alive = bool(state)
+    #指定set_alive()方法，就是将set_state()方法的state参数指定为True
+    set_alive = partialmethod(set_state, True)
+    #指定set_dead()方法，就是将set_state()方法的state参数指定为False
+    set_dead = partialmethod(set_state, False)
+
+c = Cell()
+print(c.alive)
+#相当于调用c.set_state(True)
+c.set_alive()
+print(c.alive)
+#相当于调用c.set_state(False)
+c.set_dead()
+print(c.alive)
+
+
 
 
 
