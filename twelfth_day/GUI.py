1c09e4dff034f889a313885393c812e7e4baeddf
pengshao1942 <shaopeng1942@163.com>
2020-08-28 13:59:41 +0800

    variable类改变输入框的值

--- twelfth_day/GUI.py
+++ twelfth_day/GUI.py
@@ -515,6 +515,68 @@ root.mainloop()
 
 
 
+#Variable类: 将很多GUI组件与变量进行双向绑定，执行这种双向绑定后编程非常方便
+'''
+>如果程序改变变量的值，GUI组件的显示内容或值会随之改变
+>当GUI组件的内容发生改变时(比如用户输入)，变量的值也会随之改变
+'''
+#为了让Tkinter组件与变量进行双向绑定，只要为这些组件指定variable(通常绑定组件的value)、textvariable(通常绑定组件显示的文本)等属性即可
+#Tkinter不允许将组件和普通变量进行绑定，只能和tkinter包下Variable类的子类进行绑定
+
+#Variable类的子类包含如下几个子类：
+'''
+StringVar(): 用于包装str值的变量
+IntVar(): 用于包装整型值的变量
+DoubleVar(): 用于包装浮点值的变量
+BooleanVar(): 用于包装bool值的变量
+'''
+
+#对于Variable变量：要设置其保存的变量值，则使用它的set()方法；要得到其保存的变量值，则使用它的get()方法
+
+#示例：将Entry组件与StringVar 进行双向绑定,达到既可通过StringVar改变Entry输入框显示的内容，也可通过该StringVar获取Entry输入框中的内容
+
+'''
+from tkinter import *
+#导入ttk
+from tkinter import ttk
+from tkinter import font
+
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.initWidgets()
+
+    def initWidgets(self):
+        self.st = StringVar()   #StringVar()用于包装str值的变量
+        #创建Label组件Entry(即输入框),将其textvariable绑定到self.st变量
+        ttk.Entry(self.master, textvariable=self.st, width=24, font=('StSong', 20, 'bold'),
+        foreground='red').pack(fill=BOTH, expand=YES)
+        #创建Frame作为容器
+        f = Frame(self.master)
+        f.pack()
+        #创建两个按钮，将其放入Frame中
+        ttk.Button(f, text='改变', command=self.change).pack(side=LEFT)
+        ttk,Button(f, text='获取', command=self.get).pack(side=LEFT)
+
+    def change(self):  
+        books = ('疯狂Python讲义', '疯狂Kotlin讲义', '疯狂Swift讲义')
+        import random
+        #改变self.st变量的值，与之绑定的Entry的内容随之改变
+        self.st.set(books[random.randint(0, 2)])  #variable的set()方法设置其保存的变量值
+
+    def get(self):   #定义variable的get()方法
+        from tkinter import messagebox
+        #获取self.st变量的值，实际上就是获取与之绑定的Entry中的内容
+        #并使用消息框显示self.st变量的值
+        messagebox.showinfo(title='输入内容', message=self.st.get())
+
+root = Tk()
+root.title("Variable测试")
+App(root)
+root.mainloop()
+'''
+
+
 
 
 
